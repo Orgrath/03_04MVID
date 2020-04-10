034a02a90db5d870dcdcf813fc63fbd54af9e7aa
Orgrath <josedavid_jaen@hotmail.com>
2020-04-10 14:21:21 +0200

    EJ11_01 Completed!!
    Ejercicio 01 de la tarea EJ11 ¡Completado!

---
+++ projects/EJ11_01/main.cpp
@@ -0,0 +1,209 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "engine/camera.hpp"
+#include "engine/geometry/cube.hpp"
+#include "engine/input.hpp"
+#include "engine/shader.hpp"
+#include "engine/texture.hpp"
+#include "engine/window.hpp"
+#include "engine/geometry/sphere.hpp"
+#include "engine/geometry/quad.hpp"
+
+const float velocity = 0.01f;
+
+Camera camera(glm::vec3(0.0f, 2.0f, 10.0f));
+glm::vec3 lightPos(4.0f, 1.0f, 0.0f);
+
+glm::vec3 cubePositions[] = {
+    glm::vec3(-4.0f, 0.5f, 0.0f),
+    glm::vec3(0.0f, 0.5f, 0.0f),
+    glm::vec3(4.0f, 0.5f, 0.0f),
+    glm::vec3(-2.0f, 0.5f, -3.0f),
+    glm::vec3(2.0f, 0.5f, -3.0f),
+};
+glm::vec3 grassPositions[] = {
+    glm::vec3(-5.0f, -0.1f, 1.1f),
+    glm::vec3(-4.0f, -0.1f, 1.1f),
+    glm::vec3(-3.0f, -0.1f, 1.1f),
+    glm::vec3(-2.0f, -0.1f, 1.1f),
+    glm::vec3(-1.0f, -0.1f, 1.1f),
+    glm::vec3(0.0f, -0.1f, 1.1f),
+    glm::vec3(1.0f, -0.1f, 1.1f),
+    glm::vec3(2.0f, -0.1f, 1.1f),
+    glm::vec3(3.0f, -0.1f, 1.1f),
+    glm::vec3(4.0f, -0.1f, 1.1f),
+    glm::vec3(5.0f, -0.1f, 1.1f),
+    glm::vec3(-5.5f, -0.1f, 1.3f),
+    glm::vec3(-4.5f, -0.1f, 1.3f),
+    glm::vec3(-3.5f, -0.1f, 1.3f),
+    glm::vec3(-2.5f, -0.1f, 1.3f),
+    glm::vec3(-1.5f, -0.1f, 1.3f),
+    glm::vec3(-0.5f, -0.1f, 1.3f),
+    glm::vec3(0.5f, -0.1f, 1.3f),
+    glm::vec3(1.5f, -0.1f, 1.3f),
+    glm::vec3(2.5f, -0.1f, 1.3f),
+    glm::vec3(3.5f, -0.1f, 1.3f),
+    glm::vec3(4.5f, -0.1f, 1.3f),
+    glm::vec3(5.5f, -0.1f, 1.3f)
+};
+
+float lastFrame = 0.0f;
+float lastX, lastY;
+bool firstMouse = true;
+
+void handleInput(float dt) {
+    Input* input = Input::instance();
+
+    if (input->isKeyPressed(GLFW_KEY_W)) {
+        camera.handleKeyboard(Camera::Movement::Forward, dt + velocity);
+    }
+    if (input->isKeyPressed(GLFW_KEY_S)) {
+        camera.handleKeyboard(Camera::Movement::Backward, dt + velocity);
+    }
+    if (input->isKeyPressed(GLFW_KEY_A)) {
+        camera.handleKeyboard(Camera::Movement::Left, dt + velocity);
+    }
+    if (input->isKeyPressed(GLFW_KEY_D)) {
+        camera.handleKeyboard(Camera::Movement::Right, dt + velocity);
+    }
+    if (input->isKeyPressed(GLFW_KEY_SPACE)) {
+        camera.handleKeyboard(Camera::Movement::Up, dt + velocity);
+    }
+    if (input->isKeyPressed(GLFW_KEY_LEFT_CONTROL)) {
+        camera.handleKeyboard(Camera::Movement::Down, dt + velocity);
+    }
+}
+
+void onKeyPress(int key, int action) {
+    if (key == GLFW_KEY_Q && action == GLFW_PRESS) {
+        Window::instance()->setCaptureMode(true);
+    }
+
+    if (key == GLFW_KEY_E && action == GLFW_PRESS) {
+        Window::instance()->setCaptureMode(false);
+    }
+}
+
+void onMouseMoved(float x, float y) {
+    if (firstMouse) {
+        firstMouse = false;
+        lastX = x;
+        lastY = y;
+    }
+
+    const float xoffset = x - lastX;
+    const float yoffset = lastY - y;
+    lastX = x;
+    lastY = y;
+
+    camera.handleMouseMovement(xoffset, yoffset);
+}
+
+void onScrollMoved(float x, float y) {
+    camera.handleMouseScroll(y);
+}
+
+void render(const Geometry& floor, const Geometry& object, const Shader& s_phong, const Shader& s_blend,
+    const Texture& t_floor_albedo, const Texture& t_floor_specular, const Texture& t_box_albedo, const Texture& t_box_specular, const Texture& t_tree) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glm::mat4 view = camera.getViewMatrix();
+    glm::mat4 proj = glm::perspective(glm::radians(camera.getFOV()), 800.0f / 600.0f, 0.1f, 100.0f);
+
+    //Floor
+    s_phong.use();
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(0.0f, -0.5f, 0.0f));
+    model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    model = glm::scale(model, glm::vec3(12.0f, 12.0f, 12.0f));
+    s_phong.set("model", model);
+    s_phong.set("view", view);
+    s_phong.set("proj", proj);
+
+    glm::mat3 normalMat = glm::inverse(glm::transpose(glm::mat3(model)));
+    s_phong.set("normalMat", normalMat);
+
+    s_phong.set("viewPos", camera.getPosition());
+
+    s_phong.set("light.direction", -0.2f, -1.0f, -0.3f);
+    s_phong.set("light.ambient", 0.1f, 0.1f, 0.1f);
+    s_phong.set("light.diffuse", 0.5f, 0.5f, 0.5f);
+    s_phong.set("light.specular", 1.0f, 1.0f, 1.0f);
+
+    t_floor_albedo.use(s_phong, "material.diffuse", 0);
+    t_floor_specular.use(s_phong, "material.specular", 1);
+    s_phong.set("material.shininess", 32);
+
+    floor.render();
+
+    //Boxs
+    for (const auto& cubePos : cubePositions) {
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, cubePos);
+        s_phong.set("model", model);
+
+        normalMat = glm::inverse(glm::transpose(glm::mat3(model)));
+        s_phong.set("normalMat", normalMat);
+        t_box_albedo.use(s_phong, "material.diffuse", 0);
+        t_box_specular.use(s_phong, "material.specular", 1);
+
+        object.render();
+    }
+
+    //Grasses
+    for (const auto& grassPos : grassPositions) {
+        s_blend.use();
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, grassPos);
+        s_blend.set("model", model);
+        s_blend.set("view", view);
+        s_blend.set("proj", proj);
+
+        t_tree.use(s_blend, "texture1", 0);
+
+        floor.render();
+    }
+}
+
+int main(int, char* []) {
+    Window* window = Window::instance();
+
+    glClearColor(0.0f, 0.3f, 0.6f, 1.0f);
+
+    const Shader s_phong("../projects/EJ11_01/phong.vs", "../projects/EJ11_01/blinn.fs");
+    const Shader s_blend("../projects/EJ11_01/blend.vs", "../projects/EJ11_01/blend.fs");
+    const Texture t_floor_albedo("../assets/textures/bricks_albedo.png", Texture::Format::RGB);
+    const Texture t_floor_specular("../assets/textures/bricks_specular.png", Texture::Format::RGB);
+    const Texture t_wood_albedo("../assets/textures/wood_albedo.jpg", Texture::Format::RGB);
+    const Texture t_wood_specular("../assets/textures/wood_specular.png", Texture::Format::RGB);
+    const Texture t_grass("../assets/textures/grass.png", Texture::Format::RGBA);
+    const Cube cube(2.0f);
+    const Quad quad(1.0f);
+
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    Input::instance()->setKeyPressedCallback(onKeyPress);
+    Input::instance()->setMouseMoveCallback(onMouseMoved);
+    Input::instance()->setScrollMoveCallback(onScrollMoved);
+
+    while (window->alive()) {
+        const float currentFrame = glfwGetTime();
+        const float deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        handleInput(deltaTime);
+        render(quad, cube, s_phong, s_blend, t_floor_albedo, t_floor_specular, t_wood_albedo, t_wood_specular, t_grass);
+        window->frame();
+    }
+
+    return 0;
+}
